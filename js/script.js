7f0aa0f9ed7cbbad519cb1592328acacfeaa7357
inseong01 <insung940@naver.com>
2024-06-09 15:58:11 +0900

    API key 숨김

--- js/script.js
+++ js/script.js
@@ -233,11 +233,12 @@ let compareDate;
 let secNumber = 2;
 let clicked = false;
 // -------------------
-const options = { // 초기설정
+// api key 숨김
+const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzM1MWVlZjU4MmRjODRjNmZjMTdkMTVjN2Q1ZjQ0NSIsInN1YiI6IjY2NDAyNTQwOTM2OWJiNmU2NTIwMzg5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2_g9mjUwn6IU2zyAsXws_J2S4dbafz3sj5lz-OumHag'
+    Authorization: config.Authorization,
   }
 };
 const details = {
